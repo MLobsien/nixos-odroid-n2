39281c991cd8e892195c7c80950649fac4136ae6
mlobsien <mlobsien@tutamail.com>
2026-03-19 22:10:40 +0100

    fix: add populate firmware command

--- default.nix
+++ default.nix
@@ -29,12 +29,11 @@
 
   hardware.deviceTree.name = "amlogic/meson-g12b-odroid-n2-plus.dtb";
 
-  # Is this necessary?
-  # sdImage.populateFirmwareCommands = let
-  #   configTxt = pkgs.writeText "README" ''
-  #     Nothing to see here. This empty partition is here because I don't know how to turn its creation off.
-  #   '';
-  # in ''
-  #   cp ${configTxt} firmware/README
-  # '';
+  sdImage.populateFirmwareCommands = let
+    configTxt = pkgs.writeText "README" ''
+      Nothing to see here. This empty partition is here because I don't know how to turn its creation off.
+    '';
+  in ''
+    cp ${configTxt} firmware/README
+  '';
 }
