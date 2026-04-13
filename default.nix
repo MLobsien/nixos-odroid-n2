5e6238b701d015ffe4aeea3cc67b6d6c232aba6b
MLobsien <MLobsien@tutamail.com>
2026-04-13 16:53:32 +0200

    wip

--- default.nix
+++ default.nix
@@ -1,5 +1,6 @@
 {
   pkgs,
+  lib,
   modulesPath,
   ...
 }: {
@@ -9,4 +10,16 @@
     (modulesPath + "/installer/sd-card/sd-image.nix")
     ./kboot-conf.nix
   ];
+
+  boot.loader.grub.enable = lib.mkForce false;
+  boot.consoleLogLevel = lib.mkDefault 7;
+  boot.kernelPackages = lib.mkDefault pkgs.linuxPackages_latest;
+  boot.kernelParams = lib.mkDefault [
+    "console=ttyAML0,115200n8"
+    "fsck.fix=yes"
+    "fsck.repair=yes"
+    "hdmitx=cec3f"
+  ];
+
+  hardware.deviceTree.name = lib.mkDefault "amlogic/meson-g12b-odroid-n2.dtb";
 }
